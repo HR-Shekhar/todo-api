80188ed6b9eec2fdc8c7bc07aad5e9293a45efe3
HR-Shekhar <mehimanshusss@gmail.com>
2026-05-29 23:37:40 +0530

    basic wiring

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"github.com/HR-Shekhar/todo-api/internal/config"
+	"github.com/HR-Shekhar/todo-api/internal/database"
 	"github.com/HR-Shekhar/todo-api/internal/routes"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	e := echo.New()
 	cfg := config.LoadConfig()
+	
+	e := echo.New()
+
+	dbpool := database.NewPostgresConnection(cfg)
+	defer dbpool.Close()
+
 	routes.RegisterRoutes(e)
 	
 	if err := e.Start(":"+ cfg.Port); err != nil {
